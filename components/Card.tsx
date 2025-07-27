1ccf5e62b9b8d26078a5dd0fa801812044dba1fd
Simran-Kumari92 <simranku20@gmail.com>
2025-07-27 17:10:06 +0530

    add dynamic file routes

---
+++ components/Card.tsx
@@ -0,0 +1,10 @@
+import { Models } from "node-appwrite";
+
+
+const Card = ({ file }: {file: Models.Document }) => {
+  return (
+    <div>{file.name}</div>
+  )
+}
+
+export default Card;
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,11 +1,12 @@
 'use server';
 
 import { revalidatePath } from "next/cache";
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { InputFile } from "node-appwrite/file";
 import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
+import { getCurrentUser } from "./user.actions";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -61,4 +62,48 @@ export const uploadFile = async ({
 };
 
 
+const createQueries = (
+    currentUser: Models.Document,
+    types: string[],
+    searchText: string,
+    sort: string,
+    limit?: number,
+  ) => {
+    const queries = [
+      Query.or([
+        Query.equal("owner", [currentUser.$id]),
+        Query.contains("users", [currentUser.email]),
+      ]),
+    ];
+
+    // TODO: Search, sort, limits....
+
+    return queries;
+  };
+
+
+export const getFiles = async () => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) throw new Error("User not found");
+
+    const queries = createQueries(currentUser);
+
+    console.log({currentUser, queries});
+
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      queries,
+    );
+
+    console.log({ files });
+    return parseStringify(files);
+  } catch (error) {
+    handleError(error, "Failed to get files");
+  }
+};
 
